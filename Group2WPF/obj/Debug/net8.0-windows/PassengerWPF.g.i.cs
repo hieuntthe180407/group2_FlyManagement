2a0dbb70c4604c32eb92354d019d5effdf7a6233
hieuntthe180407 <147322462+hieuntthe180407@users.noreply.github.com>
2024-12-12 22:14:30 +0700

    cache

--- Group2WPF/obj/Debug/net8.0-windows/PassengerWPF.g.i.cs
+++ Group2WPF/obj/Debug/net8.0-windows/PassengerWPF.g.i.cs
@@ -191,13 +191,13 @@ namespace Group2WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Group2WPF;V1.0.0.0;component/passengerwpf.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Group2WPF;component/passengerwpf.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\PassengerWPF.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -207,7 +207,7 @@ namespace Group2WPF {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
